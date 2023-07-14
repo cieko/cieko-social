1769f839bb2d5bcf5eed4b1af5072ee7b37a804a
cieko <rahulsaxena2021@hotmail.com>
2023-07-14 22:20:02 +0530

    Side Navbar Menu

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -1,12 +1,15 @@
 <template>
-    <TopNav />
+  <TopNav />
+  <div
+    :class="route.fullPath === '/' ? 'max-w-[1140px]' : ''"
+    class="flex justify-between mx-auto w-full lg:px-2.5 px-0"
+  >
     <div>
-
+      <SideNavMain />
     </div>
+  </div>
 </template>
 
-<script>
-    export default {
-        
-    }
+<script setup>
+const route = useRoute();
 </script>
